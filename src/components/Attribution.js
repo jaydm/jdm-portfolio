b59f222693bfb744a7c776d97c76fb711eab1287
Jay D. McHugh <jaydmchugh@gmail.com>
2019-04-19 19:10:23 -0500

    Starting to create the framework components and adding logo and favicon

---
+++ src/components/Attribution.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Attribution() {
+  return (
+    <div className="Attribution">
+    </div>
+  );
+}
+
+export default Attribution;
---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './eveningSky.png';
+
+function Logo() {
+  return (
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Logo;
